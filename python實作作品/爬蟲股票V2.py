c29a925759c72695b8900eb3f8e5cd8fca8e01e8
kevin930808 <52907743+kevin930808@users.noreply.github.com>
2022-03-30 13:42:22 +0800

    Update 爬蟲股票V2.py

--- python實作作品/爬蟲股票V2.py
+++ python實作作品/爬蟲股票V2.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+#以上為導入requests 及 BeautifulSoup的套件
 url = input()
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'lxml')
@@ -7,24 +8,24 @@ soup = BeautifulSoup(r.text, 'lxml')
 infos = soup.find('div',{'class':"D(f) Ai(fe) Mb(4px)"}).find_all('span')
 #註1
 price= infos[0].text
+#這邊的做法是不管是漲跌或是漲停板都可以抓到數值
+name=soup.select('h1[class="C($c-link-text) Fw(b) Fz(24px) Mend(8px)"]')[0].text
+num=soup.select('span[class="C($c-icon) Fz(24px) Mend(20px)"]')[0].text
+time=soup.select('span[class="C(#6e7780) Fz(12px) Fw(b)"]')[0].text
+print('為',time)
+print('該標的為',name,num)
+print('目前價位為',price)
+ #指定時間、個股名稱、個股代碼、目前價位，並print出來
 try:
-    name = soup.select('h1[class="C($c-link-text) Fw(b) Fz(24px) Mend(8px)"]')[0].text
-    num = soup.select('span[class="C($c-icon) Fz(24px) Mend(20px)"]')[0].text
+    
     down = soup.select('span[class="Fz(20px) Fw(b) Lh(1.2) Mend(4px) D(f) Ai(c) C($c-trend-down)"]')[0].text
     percents= soup.select('span[class="Jc(fe) Fz(20px) Lh(1.2) Fw(b) D(f) Ai(c) C($c-trend-down)"]')[0].text
-    print('該標的為')
-    print(name,num)
-    print('價錢為')
-    print(price,'跌',down,percents)
-    #指定個股名稱、股票代碼、跌幅、漲跌的百分比，並print出來
+    #指定跌幅、漲跌的百分比，並print出來
+    print('跌',down,'元',percents)
+    
 except:
-    name = soup.select('h1[class="C($c-link-text) Fw(b) Fz(24px) Mend(8px)"]')[0].text
-    num = soup.select('span[class="C($c-icon) Fz(24px) Mend(20px)"]')[0].text
     up = soup.select('span[class="Fz(20px) Fw(b) Lh(1.2) Mend(4px) D(f) Ai(c) C($c-trend-up)"]')[0].text
     percents= soup.select('span[class="Jc(fe) Fz(20px) Lh(1.2) Fw(b) D(f) Ai(c) C($c-trend-up)"]')[0].text
-    print('該標的為')
-    print(name,num)
-    print('價錢為')
-    print(price,'漲',up,percents)
-    #指定個股名稱、股票代碼、漲幅、漲跌的百分比，並print出來
- #####東山高中三年信班朱晨愷v2
+    print('漲',up,'元',percents)
+    #指定跌幅、漲跌的百分比，並print出來
+#####東山高中三年信班朱晨愷v2#####
